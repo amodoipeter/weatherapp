8b5dc2c581e06287ff8f0039db103463004d9f5a
amodoipeter <amodoipeter@gmail.com>
2020-11-15 18:07:50 +0300

    Added User Interface

---
+++ app/src/main/java/com/moringaschool/weatherapp/WeatherActivity.java
@@ -0,0 +1,8 @@
+package com.moringaschool.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    private CurrentWeatherService currentWeatherService;
+
+}
