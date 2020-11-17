7d4808caf6cb8d7005cf9ca891237c98dfcddfca
amodoipeter <amodoipeter@gmail.com>
2020-11-17 23:13:30 +0300

    Add onCreate metheod and initialization code

--- app/src/main/java/com/moringaschool/weatherapp/WeatherActivity.java
+++ app/src/main/java/com/moringaschool/weatherapp/WeatherActivity.java
@@ -1,8 +1,45 @@
 package com.moringaschool.weatherapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class WeatherActivity extends AppCompatActivity {
     private CurrentWeatherService currentWeatherService;
+    private View weatherContainer;
+    private ProgressBar weatherProgressBar;
+    private TextView temparature, location, weatherCondition;
+    private ImageView weatherConditionIcon;
+    private EditText locationField;
+    private FloatingActionButton fab;
 
+    private int texCount = 0
+    private boolean fetchingWeather = false;
+    private String currentLocation = "Nairobi";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_activity);
+        currentWeatherService = new CurrentWeatherService(this);
+
+        weatherContainer = findViewById(R.id.weather_container);
+        weatherProgressBar = findViewById(R.id.weather_progress_bar);
+        temparature = findViewById(R.id.temperature);
+        location = findViewById(R.id.location);
+        weatherCondition = findViewById(R.id.weather_condition);
+        weatherConditionIcon = findViewById(R.id.weather_condition_icon);
+        locationField = findViewById(R.id.location_field);
+        fab = findViewById(R.id.fab);
+
+    }
 }
