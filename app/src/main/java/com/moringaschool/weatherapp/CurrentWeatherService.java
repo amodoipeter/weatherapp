2f92d9ee2706fe5d479ddb4469c0a774cac9f441
amodoipeter <amodoipeter@gmail.com>
2020-11-18 11:02:09 +0300

    Add class

--- app/src/main/java/com/moringaschool/weatherapp/CurrentWeatherService.java
+++ app/src/main/java/com/moringaschool/weatherapp/CurrentWeatherService.java
@@ -1,4 +1,12 @@
 package com.moringaschool.weatherapp;
 
 public class CurrentWeatherService {
-}
+
+        private static final String TAG = CurrentWeatherService.class.getSimpleName();
+
+        private static final String URL = "https://api.openweathermap.org/data/2.5/weather";
+        private static final String CURRENT_WEATHER_TAG = "CURRENT_WEATHER";
+        private static final String API_KEY = "YOUR API KEY HERE"; <--- INSERT API KEY HERE
+
+        private RequestQueue queue;
+    }
