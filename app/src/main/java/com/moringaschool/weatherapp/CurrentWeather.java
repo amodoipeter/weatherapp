9bd974a142a8fced3da0634e158c53b324353572
amodoipeter <amodoipeter@gmail.com>
2020-11-18 10:17:11 +0300

    Add weather condition class and initialization code

--- app/src/main/java/com/moringaschool/weatherapp/CurrentWeather.java
+++ app/src/main/java/com/moringaschool/weatherapp/CurrentWeather.java
@@ -1,4 +1,25 @@
 package com.moringaschool.weatherapp;
 
 public class CurrentWeather {
-}
+
+    public class CurrentWeather {
+        final String location;
+        final int conditionId;
+        final String weatherCondition;
+        final double tempKelvin;
+
+        public CurrentWeather(final String location,
+                              final int conditionId,
+                              final String weatherCondition,
+                              final double tempKelvin) {
+            this.location = location;
+            this.conditionId = conditionId;
+            this.weatherCondition = weatherCondition;
+            this.tempKelvin = tempKelvin;
+        }
+
+        public int getTempFahrenheit() {
+            return (int) (tempKelvin * 9/5 - 459.67);
+        }
+
+    }
