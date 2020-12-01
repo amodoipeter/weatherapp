90535586eda3973e385703eae79d6880360792f4
amodoipeter <amodoipeter@gmail.com>
2020-12-02 00:48:35 +0300

    Add signup and sign in page with firebase authentication

---
+++ app/src/main/java/com/moringaschool/weatherapp/LoginActivity.java
@@ -0,0 +1,92 @@
+package com.moringaschool.weatherapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * @author moringa
+ */
+public class LoginActivity extends AppCompatActivity {
+    EditText emailId, password;
+    Button btnSignIn;
+    TextView tvSignUp;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignIn = findViewById(R.id.button2);
+        tvSignUp = findViewById(R.id.textView);
+
+        mAuthStateListener = firebaseAuth -> {
+            FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+            if( mFirebaseUser != null ){
+                Toast.makeText(LoginActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(LoginActivity.this, WeatherActivity.class);
+                startActivity(i);
+            }
+            else{
+                Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        btnSignIn.setOnClickListener(v -> {
+            String email = emailId.getText().toString();
+            String pwd = password.getText().toString();
+            if(email.isEmpty()){
+                emailId.setError("Please enter email id");
+                emailId.requestFocus();
+            }
+            else  if(pwd.isEmpty()){
+                password.setError("Please enter your password");
+                password.requestFocus();
+            }
+            else  if(email.isEmpty() && pwd.isEmpty()){
+                Toast.makeText(LoginActivity.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+            }
+            else  if(!(email.isEmpty() && pwd.isEmpty())){
+                mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, task -> {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Intent intToHome = new Intent(LoginActivity.this, WeatherActivity.class);
+                        startActivity(intToHome);
+                    }
+                });
+            }
+            else{
+                Toast.makeText(LoginActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        tvSignUp.setOnClickListener(v -> {
+            Intent intSignUp = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intSignUp);
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+}
